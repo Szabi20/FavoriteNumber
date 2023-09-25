296d9df9aae508bb52953ed2f2c662ba18b70e35
szt2GémesSzabolcs <115459925+Szabi20@users.noreply.github.com>
2023-09-25 11:59:26 +0200

    Add files via upload

---
+++ FavoriteNumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class FavoriteNumber {
+    public static void main(String[] args){
+
+        int szam =8;
+        System.out.println("My favorite number is: "+szam);
+
+
+
+    }}
+
